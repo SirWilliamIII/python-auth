4339bf0edd10b5401c2159c80d9ff7e514d0ecb8
Will Carpenter <willcarpenterwastaken@gmail.com>
2024-12-21 00:06:15 -0600

    added fingerprinting

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, make_response
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
@@ -27,6 +27,23 @@ app.register_blueprint(home_bp)
 
 @app.route('/')
 def index():
+    client_ip = request.remote_addr
+    user_agent = request.headers.get('User-Agent')
+    accept_language = request.headers.get('Accept-Language')
+    referer = request.headers.get('Referer', 'None')
+    cookies = request.headers.get('Cookie', 'None')
+    fingerprint = {
+        'ip': client_ip,
+        'user_agent': user_agent,
+        'accept_language': accept_language,
+        'referer': referer,
+        'cookies': cookies,
+    }
+    response = make_response("Cookie is set")
+    response.set_cookie('client_id', 'unique_id', max_age=30*24*60*60)
+    print(response.get_data)
+    print(f"Client fingerprint: {fingerprint}")
+
     return render_template('index.html')
 
 
